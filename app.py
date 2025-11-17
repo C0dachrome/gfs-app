cd8c69bacc828fc212058592a0184c4ddc6b71bb
C0dachrome <132285309+C0dachrome@users.noreply.github.com>
2025-11-17 13:59:28 -0700

    Rename Flask app variable to 'application'

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(_name_)
+application = Flask(_name_)
 
-@app.route("/")
+@application.route("/")
 def home:
     return "hello"
 if __name__ == "__main__":
-    app.run()
+    application.run()
